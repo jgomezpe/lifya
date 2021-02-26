5c89b704c5a8815f575ec36154fcf950aef5dbbf
Jonatan Gomez <jogoper@gmail.com>
2021-02-25 22:05:37 -0500

    Add js versions

--- js/lifya_wrap.js
+++ js/lifya_wrap.js
@@ -1,9 +1,8 @@
 /**
 *
-* lifya.js
+* lifya_wrap.js
 * <P>Java Script for language processing.</P>
-* <P> Requires base64.js and kompari.js. </P>
-*
+* <P>Includes base64.js and kompari.js.</P>
 * Copyright (c) 2021 by Jonatan Gomez-Perdomo. <br>
 * All rights reserved. See <A HREF="https://github.com/jgomezpe/lifya">License</A>. <br>
 *
@@ -11,6 +10,225 @@
 * (E-mail: <A HREF="mailto:jgomezpe@unal.edu.co">jgomezpe@unal.edu.co</A> )
 * @version 1.0
 */
+
+/////// Kompari.js ////////////
+/**
+ * Determines if the first number is less than (in some order) the second number(one<two)
+ * @param one First number
+ * @param two Second number
+ * @return (one<two)
+ */
+function l2h(one,two){ return (one-two) }
+
+/**
+ * Determines if the first number is greater than (in some order) the second number(one<two)
+ * @param one First number
+ * @param two Second number
+ * @return (one>two)
+ */
+function h2l(one,two){ return (two-one) }
+
+Compare = {
+    equals(one, two){
+        if(one.equals !== undefined) return one.equals(two)
+        else return one==two
+    }    
+}
+
+/**
+ * <p>Searching algorithm for sorted arrays of objects</p>
+ * 
+ * <p>Copyright: Copyright (c) 2010</p>
+ * 
+ * @author Jonatan Gomez Perdomo
+ * @version 1.0
+ */
+class SortedSearch {
+    /**
+     * Creates a search operation for the given sorted array
+     * @param sorted Array of elements (should be sorted)
+     */
+    constructor(order, sorted){ 
+        this.order = order
+        this.sorted = sorted
+    }
+    
+    /**
+     * Searches for the position of the given element. The vector should be sorted
+     * @param x Element to be located
+     * @return The position of the given object, -1 if the given object is not in the array
+     */
+    find(x, start, end) { 
+        end = end || this.sorted.length
+        start = start || 0
+        var pos = this.findRight(x, start, end)
+        if (pos > start && this.order(x, this.sorted[pos-1]) == 0) pos--
+        else pos = -1
+        return pos
+    }
+
+    /**
+     * Determines if the sorted array contains the given element (according to the associated order)
+     * @param x Element to be located
+     * @return <i>true</i> if the element belongs to the sorted array, <i>false</i> otherwise
+     */
+    contains(x, start, end){ return (this.find(x, start, end) != -1) }
+
+    /**
+     * Searches for the position of the first element in the array that is bigger
+     * than the element given. The array should be sorted
+     * @param x Element to be located
+     * @return Position of the object that is bigger than the given element
+     */
+    findRight(x, start, end){ 
+        end = end || this.sorted.length
+        start = start || 0
+        if(end > start) {
+            var a = start
+            var b = end - 1
+            if (this.order(x, this.sorted[a]) < 0)  return start
+            if (this.order(x, this.sorted[b]) >= 0) return end
+            while (a + 1 < b) {
+                var m = Math.floor((a + b) / 2)
+                if (this.order(x, this.sorted[m]) < 0) b = m
+                else a = m
+            }
+            return b
+        }else return start
+    }
+
+    /**
+     * Searches for the position of the last element in the array that is smaller
+     * than the element given. The array should be sorted
+     * @param x Element to be located
+     * @return Position of the object that is smaller than the given element
+     */
+    findLeft(x, start, end) {
+        end = end || this.sorted.length
+        start = start || 0
+        if (end > start) {
+            var a = start
+            var b = end - 1
+            if (this.order(x, this.sorted[a]) <= 0)  return start-1
+            if (this.order(x, this.sorted[b]) > 0) return b
+            while (a + 1 < b) {
+                var m = Math.floor((a + b) / 2)
+                if (this.order(x, this.sorted[m]) <= 0) b = m
+                else a = m
+            }
+            return a
+        }else return start
+    } 
+}
+
+//////// Base64.js ///////////////
+/** Object for coding/decoding uint8 arrays tto/from byte64 strings  */
+Base64 ={
+    /**
+     * From int to char
+     */
+    i2a : ['A','B','C','D','E','F','G','H','I','J','K','L','M',
+                     'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                     'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                     'n','o','p','q','r','s','t','u','v','v','x','y','z',
+                     '0','1','2','3','4','5','6','7','8','9','+','/'],
+   
+   /** 
+    * Generates the dictionary for decodign a char to int
+    */ 
+    init(){
+        if( Base64.a2i === undefined ){
+            Base64.a2i = {}
+            for( var k=0; k<Base64.i2a.length; k++ )
+                Base64.a2i[Base64.i2a[k]] = k           
+        }        
+    },
+    
+    /**
+     * Decodes a base64 string into a uint8 array if possible
+     * @param str Base64 string
+     * @return The uint8 array encode by the base64 string if possible
+     * @throws An exception if the string does not represent a valid base64 code 
+     */
+    decode(str){
+        Base64.init()
+        var end = str.length
+        while(end>=0 && str.charAt(end-1)=='=') end--
+        if(end<2) throw '·Invalid Base64 string at· ' + end
+        var m = (end%4)
+        if(m==1) throw '·Invalid Base64 string at· ' + (end-1)
+        if(m>1) m--
+        var n = 3*Math.floor(end/4) + m
+        var blob = new Uint8Array(n)
+        var control =[[2,4,1],[4,2,1],[6,0,2]]
+        var left, right
+        var k=0
+        var c=0
+        for(var i=0; i<n; i++){
+            left = Base64.a2i[str.charAt(k)]
+            right = Base64.a2i[str.charAt(k+1)]
+            if(left===undefined || right===undefined) throw '·Invalid Base64 string at· ' + k
+            blob[i] =  (left << control[c][0]) |( right >> control[c][1])
+            k+=control[c][2]
+            c = (c+1)%3
+        } 
+        return blob
+    },
+    
+    /**
+     * Encodes a uint8 array into a base64 string if possible 
+     * @param blob uint8 array to encode
+     * @return A base64 string representation of the uint8 array
+     * @throws An exception if the argument is not a uint8 array 
+     */
+    encode(blob){
+        Base64.init()
+        if( blob.byteLength === undefined ) throw '·Not a byte array·'
+        var str=''
+        var m = (blob.length%3)
+        if(m>0) m++        
+        var n = 4*Math.floor(blob.length/3) + m
+        var k=0
+        var c=0
+        for(var i=0; i<n; i++){
+            c=i&3
+            switch(c){
+                case 0: str += Base64.i2a[blob[k]>>2]; break;
+                case 1: str += Base64.i2a[((blob[k]&3)<<4) | (blob[k+1]>>4)]; break;
+                case 2: str += Base64.i2a[((blob[k]&15)<<2) | (blob[k+1]>>6)]; break;
+                case 3: str += Base64.i2a[blob[k]&63]; break;
+            }
+            if(c!=0) k++        
+        }
+        while(m<4){
+            str+='='
+            m++
+        }     
+        return str
+    },
+    
+    /**
+     * Encodes a string into a base64 string if possible 
+     * @param str str to encode
+     * @param encoder Byte level encoder for the source string
+     * @return A base64 string representation of the string
+     * @throws An exception if the argument is not a string
+     */
+    atob(str, encoder=new TextEncoder()){
+        return Base64.enconde(encoder.encode(str))
+    },
+
+    /**
+     * Decodes a base64 string into a string if possible
+     * @param str Base64 string
+     * @param encoder Byte level encoder for the traget uint8 array
+     * @return The string encode by the base64 string if possible
+     * @throws An exception if the string does not represent a valid base64 code 
+     */
+    btoa(str, decoder=new TextDecoder()){
+        return decoder.decode(Base64.decode(str))
+    }
+}
 
 /////// Lifya.js ////////////
 class Source{   
